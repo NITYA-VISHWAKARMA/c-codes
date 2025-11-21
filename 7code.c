28009dee80bd1e6a4c4dfa162433f48dbd838f71
NITYA-VISHWAKARMA <vishwakarmanitya559@gmail.com>
2025-11-21 21:49:53 +0530

    add new code

---
+++ 7code.c
@@ -0,0 +1,19 @@
+// Program to chek whether a character is a vowel or consonant.
+
+// #include <stdio.h>
+// int main  ()
+// {
+//     char ch;
+//     printf("enter a character");
+//     scanf("%c",&ch);
+//     if (ch=='a' || ch=='e' || ch=='i'||  ch=='o' || ch=='u' 
+//     &&    ch=='A'|| ch=='E'| ch=='I'||ch='O||ch=='U')
+//     {
+//     printf("%d is vowel",ch);
+//     }
+//     else{
+//     printf("%d is cosonant",ch);
+//     }
+//      }
+
+    
