f9b9ecd6b25e49a610b6cdb946a66b536275df2b
NITYA-VISHWAKARMA <vishwakarmanitya559@gmail.com>
2025-11-22 08:03:59 +0530

    add code

---
+++ 9code.c
@@ -0,0 +1,16 @@
+// Write a program to calculate sum of two numbers.
+
+#include <stdio.h>
+// #include <conio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter two numbers:");
+    scanf("%d %d",&a,&b);
+    // int a=1;
+    // int b=2;
+    c=a+b;
+    printf("sum of %d and %d is %d" ,a,b,c);
+    // getch();
+    return 0;
+}
