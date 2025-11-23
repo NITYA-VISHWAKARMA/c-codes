19716369e8f1e6e535f8deb868de504940f00907
NITYA-VISHWAKARMA <vishwakarmanitya559@gmail.com>
2025-11-23 21:37:04 +0530

    point

---
+++ 20code.c
@@ -0,0 +1,27 @@
+// #include <stdio.h>
+// int main()
+// {
+//     int x,y;
+//     scanf("%d %d", &x,&y);
+//     if (x==0 && y==0){
+//         printf("origin\n");
+//     }
+//     else if(x==0){
+//         printf("Y-axis\n");
+//     }
+//     else if(y==0)
+//          printf("X-axis\n");
+    
+//     else if(x>0 && y>0)
+//         printf("1st quadrant");
+    
+//     else if(x<0 && y>0)
+//         printf("2nd quadrant");  
+    
+//     else if(x<0 && y<0)
+//         printf("3rd quadrant");
+    
+//     else {
+//         printf("4th quadrant");
+// }        
+// }
