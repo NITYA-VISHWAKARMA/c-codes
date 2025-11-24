440aaee491d5e85f17c60bb1869bc807dee13791
NITYA-VISHWAKARMA <vishwakarmanitya559@gmail.com>
2025-11-24 18:42:35 +0530

    1while

---
+++ 23code.c
@@ -0,0 +1,14 @@
+
+// 1. WAP to print like five times on the screen.
+
+#include <stdio.h>
+int main(){
+    int i=1;
+    scanf("%d",&i);
+    while (i<=5)
+    {
+       printf("like \n");
+       i++;
+    }
+    return 0;
+}
