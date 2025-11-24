4d55b6af7561d743f155649d869c6b25a6994dfa
NITYA-VISHWAKARMA <vishwakarmanitya559@gmail.com>
2025-11-24 20:36:04 +0530

    3while

---
+++ 25code.c
@@ -0,0 +1,15 @@
+// Print all even numbers from 1 to 50 using a while loop.
+
+#include <stdio.h>
+int main(){
+    int i=0;
+    scanf("%d",&i);
+    while (0<=50)
+    {
+        if(i%2==0){
+            printf("%d\n",&i);
+        }
+        i++;
+    }
+    return 0;
+}
