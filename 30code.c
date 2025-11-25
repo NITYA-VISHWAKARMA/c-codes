dd33bf516265472796a3912ad8b310167ecc0abc
NITYA-VISHWAKARMA <vishwakarmanitya559@gmail.com>
2025-11-25 21:08:53 +0530

    triangle

---
+++ 30code.c
@@ -0,0 +1,21 @@
+// program to print a triangle validity.
+
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    if(a<=0,b<=0,c<=0){
+        printf("Invalid sides\n");
+    }
+    if(a+b>c && a+c>b && b+c>a){}
+        else if(a==b && b==c){
+            printf("Equalateral triangle\n");
+        }   
+        else if(a==b || b==c){
+            printf("isoceles triangle\n");
+        }
+        else {
+            printf("scalene triangle\n");
+        }
+}
