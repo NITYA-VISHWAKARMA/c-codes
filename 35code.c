2a10cd7e8b61afac6f7d406dc16bcd454dd6f4f8
NITYA-VISHWAKARMA <vishwakarmanitya559@gmail.com>
2025-11-27 18:42:00 +0530

    starpattern2

---
+++ 35code.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+int main()
+{
+    int i;
+    int j;
+    for(j=0;j<=5;j++){
+        for(i=1;i<=6;i++)
+        {
+        printf("*");
+        }
+        printf("\n");
+    }
+}
