3b8b16ee9e857cb9d7c1d7dacb005284cec135a3
NITYA-VISHWAKARMA <vishwakarmanitya559@gmail.com>
2025-11-30 09:04:00 +0530

    add button

--- 21code.c
+++ 21code.c
@@ -1,23 +1,26 @@
-#include <stdio.H>
-int main()
-{
-    int m,y,days;
-    scanf("%d %d", &m &y);
-    if(m<1 && m>12){
-        printf("Invalid months");
-    }
-    if(m==1,m==3,m==7,m==10,m==12){
-        printf("31 days");
-    }else if
-        (m==4,m==5,m==6,,m==9,m==11){
-         printf("30 days");  
-        }
-    else if(y%400==0 || y%4==0 && y%100!=0){
-        printf("29 days");
-    }else 
-    {
-        printf("28 days");
-    }    
-    printf("%d\n",&days);
-    return 0;
-    }
+// #include <stdio.H>
+// int main()
+// {
+//     int m,y,days;
+//     scanf("%d %d", &m &y);
+//     if(m<1 && m>12){
+//         printf("Invalid months");
+//     }
+//     if(m==1,m==3,m==7,m==10,m==12){
+//         printf("31 days");
+//     }else if
+//         (m==4,m==5,m==6,,m==9,m==11){
+//          printf("30 days");  
+//         }
+//     else if(y%400==0 || y%4==0 && y%100!=0){
+//         printf("29 days");
+//     }else 
+//     {
+//         printf("28 days");
+//     }    
+//     printf("%d\n",&days);
+//     return 0;
+//     }
+
+
+
