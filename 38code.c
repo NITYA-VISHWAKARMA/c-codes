5de2db5a2eff9a5300c7471989a6be1cf40beb9e
NITYA-VISHWAKARMA <vishwakarmanitya559@gmail.com>
2025-12-10 19:59:39 +0530

    code

--- 38code.c
+++ 38code.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include <stdio.h>
 #include <stdlib.h>
 int main()
@@ -21,3 +22,28 @@ int main()
         break;
     }
 }
+=======
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int x;
+    printf("enter a number:");
+    scanf("%d",&x);
+    switch (x)
+{
+    case 10:
+        printf("A");
+        break;
+    case 23:
+        printf("B");
+        break;   
+    case -45:
+        printf("C");
+        break;
+    default:
+        printf("default");
+        break;
+    }
+}
+>>>>>>> 4635508 (code)
