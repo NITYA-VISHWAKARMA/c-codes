4db504bf2312fe53c972c9bacf7be0ca4aa24dd5
NITYA-VISHWAKARMA <vishwakarmanitya559@gmail.com>
2025-12-10 19:40:14 +0530

    Update 38code.c
    switch

---
+++ 38code.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int x;
+    printf("enter a number:");
+    scanf("%d",&x);
+    switch (x)
+{
+    case 10:
+        printf("A");
+        break;
+    case 23:
+        printf("B");
+        break;   
+    case -45:
+        printf("C");
+        break;
+    default:
+        printf("default");
+        break;
+    }
+}
