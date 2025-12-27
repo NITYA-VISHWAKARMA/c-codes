991c4077639fe83b4e82c8ddd05f951b76e641a5
NITYA-VISHWAKARMA <vishwakarmanitya559@gmail.com>
2025-12-27 18:51:30 +0530

    increase

---
+++ 48code.c
@@ -0,0 +1,15 @@
+
+// basic increasing number pattern
+
+#include<stdio.h>
+int main(){
+    int i;
+    int j;
+
+    for (i =1;i<=5;i++){
+        for(j=1;j<=i;j++){
+        printf("%d",j);
+    }
+    printf("\n");
+} 
+}
