210fc987e4a6943d25f412159d215527d4a536de
NITYA-VISHWAKARMA <vishwakarmanitya559@gmail.com>
2026-01-08 08:12:45 +0530

    array

---
+++ 52.code.c
@@ -0,0 +1,11 @@
+// WAP to store 5 integers in an array and print them
+#include<stdio.h>
+int main()
+{
+    int a[5] = {10,20,30,40,50};
+    int i;
+    for(i=0;i<5;i++){
+        printf("%d\n" ,a[i]);
+    }
+    return 0;
+}
