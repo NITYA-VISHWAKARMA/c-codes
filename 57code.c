7114b690d90db6de28887884d8ee1b979c51f209
NITYA-VISHWAKARMA <vishwakarmanitya559@gmail.com>
2026-01-12 19:17:11 +0530

    reverse square

---
+++ 57code.c
@@ -0,0 +1,14 @@
+// WAP to print the squares of first 10 natural numbers in reverse order.
+
+
+#include<stdio.h>
+int main()
+{
+    int i=10;
+    while (i>=1)
+    {
+        printf("%d ",i*i);
+        i--;
+    }
+    printf("\n");
+}
