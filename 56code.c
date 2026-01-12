30d3a7da21367d029c6c1c39b1cee0813b8d538f
NITYA-VISHWAKARMA <vishwakarmanitya559@gmail.com>
2026-01-12 19:13:53 +0530

    square

---
+++ 56code.c
@@ -0,0 +1,13 @@
+//WAP to print the squares of first 10 natural numbers .
+
+#include<stdio.h>
+int main()
+{
+    int i=1;
+    while (i<=10)
+    {
+        printf("%d ",i*i);
+        i++;
+    }
+    
+}
